35038cb5830586ef0d26445b1b17d794d5703164
luckLuis <58127103+luckLuis@users.noreply.github.com>
2019-11-23 23:17:05 -0500

    Add files via upload

---
+++ Sumatorio.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int sumatoria(int m,int n);
+
+int main(){
+int m,n;
+    cout << "Ingrese el rango inicial: ";
+    cin >> n;
+while (n < 0){
+    cout << "Ingrese el rango inicial: ";
+    cin >> n;
+        }
+if (n >= 0){
+    cout << "Ingrese el rango final: ";
+    cin >> m;
+    while (m < n){
+    cout << "Ingrese el rango final: ";
+    cin >> m;
+    }
+}
+ cout << "La suma entre el rango [" << n << "," << m << "] es: " << sumatoria(m, n);
+ 
+	return 0;
+}
+
+int sumatoria(int m, int n){
+    int suma = 0;
+    while(n <= m){
+        suma = suma + n;
+        n++;
+    } 
+return suma;
+}
+
+
